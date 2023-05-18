8901c2c87b61de657711436de8d425598cc7cd1b
Mostafizur Rahman nayem <hpbd17062022@gmail.com>
2023-05-18 15:21:17 +0600

    Primary setup  done

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import { Button } from '@mui/material'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Hello Nayem</h1>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </>
+  )
+}
+
+export default App
